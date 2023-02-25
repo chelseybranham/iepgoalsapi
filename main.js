777a219987db2ad1bc90a43b88cd37dedc057638
Chelsey Branham <chelsbran@gmail.com>
2023-02-25 16:28:39 -0700

    updated api

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const studentName = document.querySelector('input').value
     try{
-        const response = await fetch(`https://.herokuapp.com/api/${studentName}`)
+        const response = await fetch(`http://localhost:8000/api/${studentName}`)
         const data = await response.json()
-
         console.log(data)
-        document.querySelector('h2').innerText = data.goals
+        document.querySelector('h2').innerText = data.studentGoals
     }catch(error){
         console.log(error)
     }
