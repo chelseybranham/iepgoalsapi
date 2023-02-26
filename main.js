c5e1974bacffcec864826f282b5f02ef2f0b383b
Chelsey Branham <chelsbran@gmail.com>
2023-02-25 17:01:53 -0700

    append goals into html

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ async function apiRequest(){
     try{
         const response = await fetch(`http://localhost:8000/api/${studentName}`)
         const data = await response.json()
-        console.log(data)
-        document.querySelector('h2').innerText = data.studentGoals
-    }catch(error){
+        const goalsList = document.createElement('ul')
+        for (let goal in data) {
+            const goalItem = document.createElement('li')
+            goalItem.innerText = `${goal}: ${data[goal]}`
+            goalsList.appendChild(goalItem)
+        }
+        document.querySelector('h2').innerText = `Goals for ${studentName}:`
+        document.querySelector('h2').appendChild(goalsList)
+    } catch(error){
         console.log(error)
     }
 }
